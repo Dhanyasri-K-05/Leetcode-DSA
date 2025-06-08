4b2ec690ee099ac79bffa6e3373ff8711ca0e1c8
Dhanyasri-K-05 <155838488+Dhanyasri-K-05@users.noreply.github.com>
2025-06-08 18:15:35 +0530

    Time: 21 ms (12.11%), Space: 48.8 MB (26.69%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            arr.add(i);
+        }
+        Collections.sort(arr,(a,b)-> String.valueOf(a).compareTo(String.valueOf(b)));
+
+        return arr;
+    }
+}
