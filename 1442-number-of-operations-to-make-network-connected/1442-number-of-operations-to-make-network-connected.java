e6dcbba91d2e4f6213e54b5ebac34e2437f888c9
Dhanyasri-K-05 <155838488+Dhanyasri-K-05@users.noreply.github.com>
2025-06-28 12:10:21 +0530

    Time: 14 ms (34.26%), Space: 62.9 MB (41.76%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
@@ -1,70 +1,75 @@
-class Disjoint{
-    List<Integer> parent=new ArrayList<>();
-     List<Integer> rank=new ArrayList<>();
 
-     Disjoint(int n){
+class Disjoint{
+    ArrayList<Integer> parent=new ArrayList<>();
+    ArrayList<Integer> rank=new ArrayList<>();
+   
+      Disjoint(int n){
         for(int i=0;i<n;i++){
             parent.add(i);
             rank.add(0);
         }
-     }
-
-     int findParent(int n){
-        if(parent.get(n)==n) return n;
-
-        int ulp=findParent(parent.get(n));
-        parent.set(n,ulp);
-        return parent.get(n);
-
-     }
+      }
 
 
-     void union(int u,int v){
 
-        int ulp_u=findParent(u);
-        int ulp_v=findParent(v);
 
-        if(ulp_u==ulp_v) return;
-        if(rank.get(ulp_u)<rank.get(ulp_v)){
-            parent.set(ulp_u,ulp_v);
-        }else if(rank.get(ulp_v)<rank.get(ulp_u)){
-            parent.set(ulp_v,ulp_u);
-        }else{
-            parent.set(ulp_v,ulp_u);
-            int curr=rank.get(ulp_u);
-            rank.set(ulp_u,curr+1);
-            
-        }
+      int find_parent(int u){
 
-     }
+        if(u==parent.get(u)) return u;
+
+             int up=find_parent(parent.get(u));
+             parent.set(u,up);
+             return up;
+      }
+
+
+      void union(int u,int v){
+        int pu=find_parent(u);
+        int pv=find_parent(v);
+
+        if(pu==pv) return;
+      if(rank.get(pu) <rank.get(pv)){
+        parent.set(pu,pv);
+      }
+        else if(rank.get(pu) >rank.get(pv)){
+        parent.set(pv,pu);
+      }else{
+     parent.set(pu,pv);
+     rank.set(pv,rank.get(pv)+1);
+
+
+      }
+
+
+
+      }
+
+
+
 }
-
 
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-
-        int extra=0;
-        int comcount=0;
-
-        Disjoint ds=new Disjoint(n);
+int extra=0;
+int comcount=0;
+        Disjoint dis=new Disjoint(n);
 
         for(int i=0;i<connections.length;i++){
-            int u=ds.findParent(connections[i][0]);
-             int v=ds.findParent(connections[i][1]);
+            int u=connections[i][0];
+            int v=connections[i][1];
 
-             if(u==v) extra++;
-             else{
-                ds.union(u,v);
-             }
-
+            if(dis.find_parent(u)==dis.find_parent(v)) extra++;
+            else{
+                dis.union(u,v);
+            }
         }
 
         for(int i=0;i<n;i++){
-            if(ds.parent.get(i)==i) comcount++;
+            if(dis.parent.get(i)==i) comcount++;
         }
 
+
         if(extra>=comcount-1) return comcount-1;
-        
         return -1;
         
     }
